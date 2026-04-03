d4b1a82a7fdae92421d5727b9c04f8d99b6df253
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2026-04-03 11:02:46 -0400

    (refactor) organize homepage html
    -> Banner and NavigationBar components are now included in a head
    element.
    
    -> Main element will contain the img element which will have the
    store image background.
    
    (fix) problem with getting the image to be rendered in the page.

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,7 +1,19 @@
 import Banner from './Banner';
+import NavigationBar from '../navigation-bar/NavigationBar';
+import background from '../../assets/background-brush.jpg';
 
 function HomePage() {
-	return <Banner />;
+	return (
+		<>
+			<header>
+				<Banner />
+				<NavigationBar />
+			</header>
+			<main>
+				<img className={background} alt='' />
+			</main>
+		</>
+	);
 }
 
 export default HomePage;
