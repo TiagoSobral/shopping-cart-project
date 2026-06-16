f8575218c26dd73d6a3246db6b03c1af9d35e5d4
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2026-06-16 11:54:46 -0400

    (refactor) remove unnecessary info for testing

--- src/components/navigation-bar/NavigationBar.test.jsx
+++ src/components/navigation-bar/NavigationBar.test.jsx
@@ -1,27 +1,10 @@
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import NavigationBar from './NavigationBar';
 import ShopPage from '../shop-page/ShopPage';
 import App from '../App';
 import { createMemoryRouter, MemoryRouter, RouterProvider } from 'react-router';
 import { routes } from '../../routes.jsx';
-import userEvent from '@testing-library/user-event';
-
-const products = [
-	{
-		id: 'da123',
-		image_link: 'url_test',
-		name: 'name_test',
-		brand: 'brand_test',
-		description: 'description_test',
-		price: 20,
-		quantity: 1,
-	},
-];
-
-globalThis.fetch = vi.fn(
-	async () => await Promise.resolve({ json: () => products }),
-);
 
 describe('Navigation Bar', () => {
 	it('navigation bar component is rendered', () => {
@@ -50,6 +33,3 @@ describe('Navigation Bar', () => {
 		expect(cartBtn).toBeInTheDocument();
 	});
 });
-
-/* Tests: 
-- check when add product to cart if cart button becomes a number */
